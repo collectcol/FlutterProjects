51aca8bb06329ba0d3cc59c240571bfe4e5e62a7
collectcol <sinwookyoung15@gmail.com>
2023-11-26 22:01:38 +0900

    2023-11-26 second

--- inflean/vid_player/lib/screen/home_screen.dart
+++ inflean/vid_player/lib/screen/home_screen.dart
@@ -1,15 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
+      body: renderEmpty(),
+    );
+  }
+
+  Widget renderEmpty() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: getBoxDecoration(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(onTap: onLogoTap),
+          const SizedBox(
+            height: 30.0,
+          ),
+          const _AppName(),
+        ],
+      ),
+    );
+  }
+
+  void onLogoTap() async {
+    await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter, // 색깔의 시작점
+        end: Alignment.bottomCenter, // 색깔의 종료점
+        colors: [
+          Color(0XFF2A3A7C),
+          Color(0XFF000118),
+        ],
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  final VoidCallback onTap;
+  const _Logo({required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset(
+        'asset/image/logo.png',
+      ),
+    );
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName();
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image.asset('asset/image/logo.png'),
+        const Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
       ],
-    ));
+    );
   }
 }
